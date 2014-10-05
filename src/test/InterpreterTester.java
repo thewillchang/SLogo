3a4db56d5da258db83186661ae38aa87fc186324
thewillchang <wkc10@duke.edu>
2014-10-04 21:10:12 -0400

    Created Parser and Interpreter Testers

---
+++ src/test/InterpreterTester.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+import org.junit.AfterClass;
+import org.junit.Test;
+
+
+public class InterpreterTester {
+
+    @Test
+    public void test() {
+            fail("Not yet implemented");
+    }
+}
