aa1ea7d397b611b3eaf1b2b878545d9f17f85d08
Jonathan Tseng <jtt24@duke.edu>
2014-10-04 20:39:31 -0400

    added test package and junit library

---
+++ src/test/SLogoExpressionFactoryTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+public class SLogoExpressionFactoryTest {
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
