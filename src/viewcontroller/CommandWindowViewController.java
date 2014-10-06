718e141522aa6e34b972dbc31a5ddfec7d29cf29
Abhishek <abhishek.bend@gmail.com>
2014-10-06 13:11:25 -0400

    Abhishek version, re-cloning project b/c of Eclipse issues - basic structure of commandwindowviewcontroller

--- src/viewcontroller/CommandWindowViewController.java
+++ src/viewcontroller/CommandWindowViewController.java
@@ -1,9 +1,15 @@
 package viewcontroller;
 
+import java.awt.Dimension;
 import java.util.Observable;
 import java.util.Observer;
 
+import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import application.Main;
 
 /**
  * ViewController for Command Windows (where command prompt, command status,
@@ -15,10 +21,12 @@ import javafx.scene.Node;
 
 public class CommandWindowViewController implements Observer, ViewController {
 
+
 	public static final Dimension SIZE = new Dimension(
 			Main.SIZE.width / 2 * 9 / 10, Main.SIZE.height * 9 / 10);
 
 	private BorderPane myPane;
+	private VBox commandWindowVerticalBox;
 	private UserDefinedMethodsWindowViewController myUserDefinedMethodsView;
 	private UserDefinedVariablesWindowViewController myUserDefinedVariablesView;
 	private HistoryWindowViewController myHistoryView;
@@ -27,7 +35,27 @@ public class CommandWindowViewController implements Observer, ViewController {
 
 	public CommandWindowViewController() {
 		myPane = new BorderPane();
+		
 		myPane.setPrefSize(SIZE.width, SIZE.height);
+		commandWindowVerticalBox = new VBox();
+		
+		HBox userDefinedHorizontalBox = placeUserDefinedBoxes();
+		myHistoryView = new HistoryWindowViewController();
+		myCommandPromptView = new CommandPromptViewController();
+		myCommandStatusView = new CommandStatusViewController();
+		
+//		commandWindowVerticalBox.
+//		commandWindowVerticalBox.addAll(/*placeUserDefinedBoxes(), */myHistoryView, myCommandPromptView, myCommandStatusView);
+	}
+	
+	private HBox placeUserDefinedBoxes() {
+		HBox userDefinedHorizontalBox = new HBox();
+		myUserDefinedMethodsView = new UserDefinedMethodsWindowViewController();
+		myUserDefinedVariablesView = new UserDefinedVariablesWindowViewController();
+		
+//		userDefinedHorizontalBox.addAll(myUserDefinedMethodsView, myUserDefinedVariablesView);
+//		return HBox;
+		return null;
 	}
 
 	@Override
@@ -39,5 +67,4 @@ public class CommandWindowViewController implements Observer, ViewController {
 	public void update(Observable o, Object arg) {
 
 	}
-
 }
