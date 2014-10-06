4dc2c3ce376e1ac0f890a7dbb0719bb90aec676c
thewillchang <wkc10@duke.edu>
2014-10-06 00:44:18 -0400

    Created methods for preprocessing the input and passing processed inputs to the Expression Factory. Began creating stacks for processing syntax.

--- src/interpreter/Parser.java
+++ src/interpreter/Parser.java
@@ -1,27 +1,73 @@
 package interpreter;
 
 import interpreter.expression.SLogoExpression;
-
+import interpreter.expression.SLogoExpressionFactory;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
-
+import java.util.Collections;
+import java.util.Deque;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
 import exceptions.SLogoParsingException;
 
 /**
  * class that parses the input and creates an SLogoExpression representing it
  * throws parsing exception if fails
- * @author Jonathan Tseng
+ * @author Will Chang and Jonathan Tseng
  *
  */
 public class Parser {
 
-	/**
-	 * reads expression from back, parsing using a stack and returns list of expressions to evaluate
-	 * @param sLogoExpression
-	 * @return
-	 * @throws SLogoParsingException
-	 */
-	public Collection<SLogoExpression> parseSLogoExpression(String sLogoExpression) throws SLogoParsingException{
-		return null;
-	}
+    private SLogoExpressionFactory myFactory;
+    private Deque<SLogoExpression> expressionStack = new ArrayDeque<>();
+    private Deque<SLogoExpression> parameterStack = new ArrayDeque<>();
+    
+
+    public Parser() {
+        myFactory = new SLogoExpressionFactory();
+    }
+    /**
+     * reads expression from back, parsing using a stack and returns list of expressions to evaluate
+     * @param input
+     * @return
+     * @throws SLogoParsingException
+     */
+    public Collection<SLogoExpression> parseSLogoExpression (String input) throws SLogoParsingException {
+
+        createExpressionsFromProcessedInput(processInput(input));
+        return null;
+    }
+
+    private Deque<String> processInput(String input) {
+        List<String> processedInputs = Arrays.asList(input.split("\\s"));
+        Collections.reverse(processedInputs);
+        return new ArrayDeque<>(processedInputs);
+    }
+
+    private Collection<SLogoExpression> createExpressionsFromProcessedInput (Deque<String> processedInputStack) 
+            throws SLogoParsingException {
+
+        while(!processedInputStack.isEmpty()) {
+            String input = processedInputStack.pop();
+            analyzeExpressionSyntax(myFactory.createExpression(input));
+        }
+
+        return parameterStack;
+    }
+
+    private void analyzeExpressionSyntax(SLogoExpression expression) {
+
+    }
+
+    public static void main(String[] args) throws SLogoParsingException
+    {
+        Parser p = new Parser();
+        String input = "forward 50";
+        p.parseSLogoExpression(input);
+    }
 
 }
