e220e7c6017a27059ef55e55312ed06343cf602a
Abhishek <abhishek.bend@gmail.com>
2014-10-05 21:23:10 -0400

    filling out CommandWindowViewController

--- src/viewcontroller/CommandWindowViewController.java
+++ src/viewcontroller/CommandWindowViewController.java
@@ -5,18 +5,39 @@ import java.util.Observer;
 
 import javafx.scene.Node;
 
+/**
+ * ViewController for Command Windows (where command prompt, command status,
+ * history, and user defined windows are located)
+ * 
+ * @author Abhishek B
+ *
+ */
+
 public class CommandWindowViewController implements Observer, ViewController {
+
+	public static final Dimension SIZE = new Dimension(
+			Main.SIZE.width / 2 * 9 / 10, Main.SIZE.height * 9 / 10);
+
+	private BorderPane myPane;
+	private UserDefinedMethodsWindowViewController myUserDefinedMethodsView;
+	private UserDefinedVariablesWindowViewController myUserDefinedVariablesView;
+	private HistoryWindowViewController myHistoryView;
+	private CommandPromptViewController myCommandPromptView;
+	private CommandStatusViewController myCommandStatusView;
+
+	public CommandWindowViewController() {
+		myPane = new BorderPane();
+		myPane.setPrefSize(SIZE.width, SIZE.height);
+	}
 
 	@Override
 	public Node getNode() {
-		// TODO Auto-generated method stub
-		return null;
+		return myPane;
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 }
