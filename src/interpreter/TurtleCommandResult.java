ae664305db44c563d9a49f492678cf918712117b
thewillchang <wkc10@duke.edu>
2014-10-06 00:41:13 -0400

    Created TurtleCommandResult as SLogoExpressionResult subclass.

---
+++ src/interpreter/TurtleCommandResult.java
@@ -0,0 +1,36 @@
+package interpreter;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import transitionstate.TransitionState;
+import exceptions.SLogoParsingException;
+
+public class TurtleCommandResult extends SLogoResult {
+    private ResourceBundle myProperties;
+    
+    public TurtleCommandResult(int value) {
+        super();
+        myValue = value;
+        
+        
+    }
+    @Override
+    public SLogoParsingException getException () {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<TransitionState> getTransition () {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int getValue () {
+        // TODO Auto-generated method stub
+        return myValue;
+    }
+
+}
