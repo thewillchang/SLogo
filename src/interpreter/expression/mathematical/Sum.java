a48aff0230af386bdaecd931a475c5a792bca931
William Chang <Will@williams-mbp-79.wireless.duke.local>
2014-10-11 02:35:10 -0400

    Recreated Sum.java file to fix naming error... Everything good to go.

---
+++ src/interpreter/expression/mathematical/Sum.java
@@ -0,0 +1,30 @@
+package interpreter.expression.mathematical;
+
+import interpreter.SLogoResult;
+import interpreter.expression.SLogoExpression;
+import interpreter.expression.TwoArgumentCommand;
+
+import java.util.Deque;
+
+import transitionstate.TransitionState;
+
+public class Sum extends TwoArgumentCommand {
+
+    private SLogoExpression operand1;
+    private SLogoExpression operand2;
+
+    @Override
+    public void loadArguments(Deque<SLogoExpression> args) {
+        super.loadArguments(args);
+    }
+
+    @Override
+    public SLogoResult evaluate() {
+
+        double value = (operand1.evaluate().getValue() + operand2.evaluate().getValue());
+        TransitionState state = new TransitionState();
+
+        return  new MathResult(value, state);
+
+    }
+}
