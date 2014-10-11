4d5562685946f0b43226fa393a466b0a790b1aa3
William Chang <Will@williams-mbp-79.wireless.duke.local>
2014-10-11 03:03:33 -0400

    Temporary fix for loading new Transition states, will refactor functionality to better fit design.

--- src/interpreter/expression/Constant.java
+++ src/interpreter/expression/Constant.java
@@ -5,11 +5,19 @@ import java.util.List;
 import interpreter.SLogoResult;
 import interpreter.SyntaxResult;
 
+/**
+ * 
+ * @author Will Chang
+ *
+ */
 public class Constant extends SyntaxExpression {
 
     private List<SLogoExpression> myParameters;
     private double myConstantValue;
     
+    public Constant () {
+        
+    }
     
     @Override
     public void loadArguments(Deque<SLogoExpression> args) {
--- src/interpreter/expression/Forward.java
+++ src/interpreter/expression/Forward.java
@@ -2,13 +2,22 @@ package interpreter.expression;
 
 import interpreter.SLogoResult;
 import interpreter.TurtleCommandResult;
+import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.List;
 import transitionstate.TransitionState;
 
+/**
+ * 
+ * @author Will Chang
+ *
+ */
 public class Forward extends TurtleCommandExpression {
     private Deque<SLogoExpression> myParameters;
 
+    public Forward () {
+        myParameters = new ArrayDeque<>();
+    }
     @Override
     public void loadArguments(Deque<SLogoExpression> args) {
         try {
@@ -22,12 +31,20 @@ public class Forward extends TurtleCommandExpression {
     @Override
     public SLogoResult evaluate() {
         SLogoResult previousResult = myParameters.pop().evaluate();
-        TransitionState prevTransition = previousResult.getTransition().get(0);
-        TransitionState nextTransition = new TransitionState(prevTransition.getPenUp(), 
+        //TODO Edit transition states to only hold delta values, that is,
+        //only hold changes that are not dependent on previous changes, only values
+        
+        //TransitionState prevTransition = previousResult.getTransition();
+        /*TransitionState nextTransition = new TransitionState(prevTransition.getPenUp(), 
                                                              prevTransition.getTurtleVisible(), 
                                                              previousResult.getValue(), 
                                                              prevTransition.getRotateClockwise(),
-                                                             prevTransition.getRotateCounterClockwise());
+                                                             prevTransition.getRotateCounterClockwise());*/
+        TransitionState nextTransition = new TransitionState(false, 
+                                                             true, 
+                                                             previousResult.getValue(), 
+                                                             0,
+                                                             0);
         SLogoResult myResult = new TurtleCommandResult(previousResult.getValue());
         myResult.getTransition().add(nextTransition);
         myResult.getTransition().addAll(previousResult.getTransition());
