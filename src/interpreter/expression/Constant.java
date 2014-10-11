13c5082516ec102c5f791598f5b06681ac573ec0
William Chang <Will@williams-mbp-79.wireless.duke.local>
2014-10-10 20:50:54 -0400

    Implemented the Constant Expression. Refactored the Forward Expression.

--- src/interpreter/expression/Constant.java
+++ src/interpreter/expression/Constant.java
@@ -1,21 +1,29 @@
 package interpreter.expression;
 
+import java.util.Deque;
 import java.util.List;
 import interpreter.SLogoResult;
+import interpreter.SyntaxResult;
 
 public class Constant extends SyntaxExpression {
 
     private List<SLogoExpression> myParameters;
+    private double myConstantValue;
+    
     
     @Override
-    public void loadArguments(List<SLogoExpression> args) {
+    public void loadArguments(Deque<SLogoExpression> args) {
         
     }
 
+    public void setValue(double value) {
+        myConstantValue = value;
+    }
+    
     @Override
     public SLogoResult evaluate() {
             // TODO Auto-generated method stub
-            return null;
+            return new SyntaxResult(myConstantValue);
     }
 
 }
