c3d5388cd5e8671de786d3f0f0faecf0f290c2fb
William Chang <Will@williams-mbp-79.wireless.duke.local>
2014-10-15 22:39:32 -0400

    Added functionality to single turtle commands.

--- src/interpreter/expression/singleturtle/ClearScreen.java
+++ src/interpreter/expression/singleturtle/ClearScreen.java
@@ -1,5 +1,13 @@
 package interpreter.expression.singleturtle;
 
+import interpreter.SLogoResult;
+import interpreter.TurtleCommandResult;
+import interpreter.expression.SLogoExpression;
+import interpreter.expression.TurtleCommandExpression;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import transitionstate.TransitionState;
+
 public class ClearScreen {
 
 }
--- src/interpreter/expression/singleturtle/PenDown.java
+++ src/interpreter/expression/singleturtle/PenDown.java
@@ -1,5 +1,37 @@
 package interpreter.expression.singleturtle;
 
-public class PenDown {
+import interpreter.SLogoResult;
+import interpreter.TurtleCommandResult;
+import interpreter.expression.SLogoExpression;
+import interpreter.expression.TurtleCommandExpression;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import transitionstate.TransitionState;
+
+public class PenDown extends TurtleCommandExpression {
+    private Deque<SLogoExpression> myParameters;
+
+    public PenDown () {
+        myParameters = new ArrayDeque<>();
+    }
+    
+    @Override
+    public void loadArguments(Deque<SLogoExpression> args) {
+    }
+
+    @Override
+    public SLogoResult evaluate() {
+        SLogoResult previousResult = myParameters.pop().evaluate();
+        TransitionState nextTransition = new TransitionState(false, 
+                                                             true, 
+                                                             0, 
+                                                             0,
+                                                             0);
+        SLogoResult myResult = new TurtleCommandResult(previousResult.getValue());
+        myResult.getTransition().add(nextTransition);
+        myResult.getTransition().addAll(previousResult.getTransition());
+        return myResult;
+
+    }
 
 }
