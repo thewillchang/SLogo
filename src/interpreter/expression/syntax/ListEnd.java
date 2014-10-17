3737fe5c8b200702a448d04b3d9b40ba6b947fd7
William Chang <Will@williams-mbp-79.wireless.duke.local>
2014-10-16 23:33:16 -0400

    Created new SLogoResult for Control Structures/Variables. Updated SLogoResult. Implemented ControlStructure expressions: ListEnd, ListStart, and Repeat. Working on If, DoTimes, MakeUserInstruction, GroupStart, GroupEnd.

--- src/interpreter/expression/syntax/ListEnd.java
+++ src/interpreter/expression/syntax/ListEnd.java
@@ -1,5 +1,25 @@
 package interpreter.expression.syntax;
 
-public class ListEnd {
+import interpreter.SLogoResult;
+import interpreter.TurtleCommandResult;
+import interpreter.expression.SLogoExpression;
+import interpreter.expression.TurtleCommandExpression;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import transitionstate.TransitionState;
+
+public class ListEnd implements SLogoExpression{
+
+    @Override
+    public void loadArguments (Deque<SLogoExpression> args) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public SLogoResult evaluate () {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
--- src/interpreter/expression/variable/If.java
+++ src/interpreter/expression/variable/If.java
@@ -1,5 +1,33 @@
 package interpreter.expression.variable;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import transitionstate.TransitionState;
+import exceptions.SLogoParsingException;
+import interpreter.ControlStructureResult;
+import interpreter.SLogoResult;
+import interpreter.expression.SLogoExpression;
+import interpreter.expression.syntax.ListEnd;
+import interpreter.expression.syntax.ListStart;
 
-public class If {
+public class If implements SLogoExpression {
+    private SLogoExpression myMaxRep;
+    private List<SLogoExpression>  myList = new ArrayList<>();
+    //if expr is not 0, runs the commands given in the list
+    //returns the value of the final command executed
+    
+    @Override
+    public void loadArguments (Deque<SLogoExpression> args) throws SLogoParsingException,
+                                                           NullPointerException {
+
+        
+    }
+
+    @Override
+    public SLogoResult evaluate () {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
