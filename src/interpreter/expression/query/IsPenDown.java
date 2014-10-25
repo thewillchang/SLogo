81a375bc08ea4b0ddccf9a92dae35482331f4067
Abhishek <abhishek.bend@gmail.com>
2014-10-25 16:31:44 -0400

    Minor regarding query expressions

--- src/interpreter/expression/query/IsPenDown.java
+++ src/interpreter/expression/query/IsPenDown.java
@@ -1,5 +1,21 @@
 package interpreter.expression.query;
 
-public class IsPenDown {
+import interpreter.CommandReferenceLibrary;
+import interpreter.expression.TurtleQueryExpression;
 
+public class IsPenDown extends TurtleQueryExpression {
+
+	@Override
+	public void setNumArgs(int value) {
+	}
+
+	@Override
+	public void loadLibrary(CommandReferenceLibrary library) {
+		
+	}
+	
+	@Override
+	public int getNumArgs(){
+		return 0;
+	}
 }
