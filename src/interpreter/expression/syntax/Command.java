63b69fc288c8ef0e2f9d7b293d0c0b2f790558d7
William Chang <Will@Williams-MacBook-Pro-79.local>
2014-10-25 04:31:19 -0400

    Implementing and refactoring the UserDefined Variables and Commands, and related expression classes.

--- src/interpreter/expression/syntax/Command.java
+++ src/interpreter/expression/syntax/Command.java
@@ -1,43 +1,64 @@
 package interpreter.expression.syntax;
 
+import java.util.ArrayList;
 import java.util.Deque;
+import java.util.List;
 import model.MainModel;
+import model.UserDefinedVariablesModel;
 import exceptions.SLogoParsingException;
 import interpreter.CommandReferenceLibrary;
 import interpreter.SLogoResult;
+import interpreter.SyntaxResult;
+import interpreter.UserDefinedResult;
 import interpreter.expression.SLogoExpression;
+import interpreter.expression.SyntaxExpression;
 
-public class Command implements SLogoExpression{
+public class Command extends SyntaxExpression {
+    private int myNumArgs;
+    private String myName;
+    private SLogoExpression myCommands;
+
+    public Command() {
+        super();
+    }
+    
+    public Command (String name, SLogoExpression commands) {
+        super();
+        setValue(name);
+        myCommands = commands;
+        
+    }
 
     @Override
-    public void loadArguments (Deque<SLogoExpression> args) throws SLogoParsingException,
-                                                           NullPointerException {
-        // TODO Auto-generated method stub
-        
+    public void loadArguments(Deque<SLogoExpression> args) {
+        if(myLibrary.getUserDefinedCommands().containsCommand(myName)) {
+            for(int i = 0; i < myNumArgs; i++) {
+                myArguments.add(args.pop());
+            }
+        }
     }
 
     @Override
     public SLogoResult evaluate () {
-        // TODO Auto-generated method stub
-        return null;
+
+
+        UserDefinedVariablesModel myUserDefinedVariables = myLibrary.getUserDefinedVariables();
+        if(myUserDefinedVariables.containsVariable(myValue)) {
+            return new UserDefinedResult(myUserDefinedVariables.getVariable(myValue));
+        }
+        return new SyntaxResult(myValue);
     }
 
     @Override
-    public void setNumArgs (int value) {
-        // TODO Auto-generated method stub
-        
+    public void setValue (String value) {
+        myName = value;
+
+    }
+    
+    @Override
+    public String toString() {
+        return myName;
     }
 
-    @Override
-    public void loadLibrary (CommandReferenceLibrary library) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void loadModel (MainModel model) {
-        // TODO Auto-generated method stub
-        
-    }
 
 }
